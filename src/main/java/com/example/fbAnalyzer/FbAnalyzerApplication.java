277941a54df05bd6aee838587f6414394ff946d3
Mark Villa <mark.villa.x@gmail.com>
2020-07-12 03:30:17 -0500

    [fbAnalysis]: 1) Project Setup 2) Spring Boot Initialization

---
+++ src/main/java/com/example/fbAnalyzer/FbAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.example.fbAnalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FbAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fbAnalyzer/FbAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fbAnalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FbAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
